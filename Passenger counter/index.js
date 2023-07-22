50ba8d0fb1520868cbd2cad3c4b5331a23f315c4
Srinidhi Balaji <132289138+burntbeans07@users.noreply.github.com>
2023-07-22 16:45:25 +0530

    Add files via upload

---
+++ Passenger counter/index.js
@@ -0,0 +1,16 @@
+let count = 0
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.innerText += countStr
+    console.log(count)
+}
+
+console.log("Let's count people on the subway!")
